d110ba6acc6aeb6cdc6190fb6cbff2c5f336681e
김지성 <tony6356@naver.com>
2026-03-02 11:08:28 +0900

    [feat] join project

--- apps/api/src/main/java/capstone/api/repository/ProjectMemberRepository.java
+++ apps/api/src/main/java/capstone/api/repository/ProjectMemberRepository.java
@@ -5,5 +5,5 @@ import capstone.api.domain.ProjectMemberId;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProjectMemberRepository extends JpaRepository<ProjectMember, ProjectMemberId> {
-    
+    boolean existsByUserIdAndProjectId(Long userId, Long projectId);
 }
--- apps/api/src/main/java/capstone/api/service/ProjectService.java
+++ apps/api/src/main/java/capstone/api/service/ProjectService.java
@@ -3,6 +3,7 @@ package capstone.api.service;
 
 import capstone.api.domain.Project;
 import capstone.api.domain.ProjectMember;
+import capstone.api.dto.JoinProjectRequest;
 import capstone.api.repository.ProjectRepository;
 import capstone.api.dto.CreateProjectRequest;
 import capstone.api.dto.ProjectResponse;
@@ -33,5 +34,19 @@ public class ProjectService {
         projectMemberRepository.save(member);
 
         return ProjectResponse.from(savedProject);
+    }
+    @Transactional
+    public void joinProject(Long userId, JoinProjectRequest request){
+        Project project = projectRepository.findByInviteCode(request.inviteCode())
+                .orElseThrow(() -> new IllegalArgumentException("유효하지 않거나 존재하지 않는 초대 코드입니다."));
+        //이미 참여중인 유저인지 확인(중복방지)
+        if(projectMemberRepository.existsByUserIdAndProjectId(userId, project.getId())){
+            throw new IllegalArgumentException("이미 참여 중인 프로젝트입니다.");
+        }
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+        ProjectMember newMember = new ProjectMember(project, user, ProjectMember.Role.PARTICIPANT);
+        projectMemberRepository.save(newMember);
+
     }
 }
