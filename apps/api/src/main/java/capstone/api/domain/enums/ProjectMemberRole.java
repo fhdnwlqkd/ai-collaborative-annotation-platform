2857fee402bac201c964dedd30cfb9fa66a2098c
김지성 <tony6356@naver.com>
2026-03-04 15:36:17 +0900

    [chore] delete Korean desciption

--- apps/api/src/main/java/capstone/api/domain/enums/ProjectMemberRole.java
+++ apps/api/src/main/java/capstone/api/domain/enums/ProjectMemberRole.java
@@ -6,8 +6,6 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum ProjectMemberRole {
-    OWNER("소유자"),
-    PARTICIPANT("참여자");
-
-    private final String description;
+    OWNER,
+    PARTICIPANT;
 }
